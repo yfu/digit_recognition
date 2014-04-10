4dab07be993d4fd07b23919f7ebcd977a563f997
Yu Fu <yfu@yfu.me>
2014-04-10 00:20:34 -0400

    Add my_NN.m

---
+++ test_example_NN.m
@@ -0,0 +1,22 @@
+function test_example_NN
+load mnist_uint8;
+
+train_x = double(train_x) / 255;
+test_x  = double(test_x)  / 255;
+train_y = double(train_y);
+test_y  = double(test_y);
+
+% normalize
+[train_x, mu, sigma] = zscore(train_x);
+test_x = normalize(test_x, mu, sigma);
+
+%% ex1 vanilla neural net
+rand('state',0)
+nn = nnsetup([784 100 10]);
+opts.numepochs =  1;   %  Number of full sweeps through data
+opts.batchsize = 100;  %  Take a mean gradient step over this many samples
+[nn, L] = nntrain(nn, train_x, train_y, opts);
+
+[er, bad] = nntest(nn, test_x, test_y);
+er
+assert(er < 0.08, 'Too big error');
